4591e0f68c11642ffcab52e9d23d25a841817329
Aryan Ratra <95769010+aryanratra@users.noreply.github.com>
2022-03-11 15:54:14 +0530

    First Commit

---
+++ LearningJava/src/Unit_01/P1_Task02_FirstJavaProgramInTerminal.java
@@ -0,0 +1,10 @@
+package Unit_01;
+
+public class P1_Task02_FirstJavaProgramInTerminal {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World. I am a terminal.");
+
+	}
+
+}
